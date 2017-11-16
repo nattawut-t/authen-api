e9f2f5948d0ef26ccdd7f7aa82e7a73e15ee7efa
j2sdk_ball2 <nattawut@moneytable.com>
2017-11-16 14:41:44 +0700

    update docker scripts

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,4 +1,4 @@
 docker rm -f authen-api
-docker rmi -f authen-api:0.0.1
-docker build -t authen-api:0.0.1 .
-docker run -d --name=authen-api -p 3000:3000 -v $(pwd):/src authen-api:0.0.1
+docker rmi -f authen-api:1.0.0
+docker build -t authen-api:1.0.0 .
+docker run -d --name=authen-api -p 3000:3000 -v $(pwd):/src authen-api:1.0.0
